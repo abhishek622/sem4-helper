5703db91ebccf6704beff43034365c90f3f63fc7
abhishek622 <abhishekprasad0602@gmail.com>
2021-07-17 10:31:15 +0530

    image change

--- src/styles/Home.style.jsx
+++ src/styles/Home.style.jsx
@@ -1,17 +1,18 @@
 import { makeStyles } from '@material-ui/core';
 
-import pic1 from '../images/pic1.svg';
+import pic3 from '../images/pic3.svg';
 
 export const useStyles = makeStyles(theme => ({
   root: {
     overflowY: 'hidden',
     height: '100vh',
-    backgroundImage: `url(${pic1})`,
+    backgroundImage: `url(${pic3})`,
     backgroundPosition: 'left bottom',
     backgroundRepeat: 'no-repeat',
     backgroundSize: '380px',
     [theme.breakpoints.down('sm')]: {
-      backgroundImage: 'none',
+      backgroundPosition: 'center bottom',
+      backgroundSize: '320px',
     },
   },
   head: {
