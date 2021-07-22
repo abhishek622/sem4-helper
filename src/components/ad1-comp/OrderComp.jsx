0f8ac88b4f4ac88ef162ce9d888bc26eb19f80b0
abhishek622 <abhishekprasad0602@gmail.com>
2021-07-22 13:27:02 +0530

    fix

--- src/components/ad1-comp/OrderComp.jsx
+++ src/components/ad1-comp/OrderComp.jsx
@@ -11,11 +11,11 @@ export default function OrderComp() {
       <div className={classes.whiteSpace} />
 
       <Typography variant="body1">
-        O&lpar;1&#41; &#x3c; O&lpar;log log n&#41; &#x3c; O&lpar;log n&#41;
-        &#x3c; O&lpar;n<sup>1/2</sup>&#41; &#x3c; O&lpar;n&#41; &#x3c; O&lpar;n
-        log n&#41; &#x3c; O&lpar;n<sup>2</sup>&#41; &#x3c; O&lpar;n<sup>k</sup>
-        &#41; &#x3c; O&lpar;2<sup>n</sup>&#41; &#x3c; O&lpar;n<sup>n</sup>&#41;
-        &#x3c; O&lpar;2
+        O&#40;1&#41; &#x3c; O&#40;log log n&#41; &#x3c; O&#40;log n&#41; &#x3c;
+        O&#40;n<sup>1/2</sup>&#41; &#x3c; O&#40;n&#41; &#x3c; O&#40;n log n&#41;
+        &#x3c; O&#40;n<sup>2</sup>&#41; &#x3c; O&#40;n<sup>k</sup>
+        &#41; &#x3c; O&#40;2<sup>n</sup>&#41; &#x3c; O&#40;n<sup>n</sup>&#41;
+        &#x3c; O&#40;2
         <sup>
           2<sup>2</sup>
         </sup>
